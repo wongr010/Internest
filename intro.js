a002a17708d99f1a9813b619d5a77b4127148023
Rosalyn <r928wong@gmail.com>
2017-06-01 12:16:25 -0400

    Edited intro slogan to fit to mobile screen

--- intro.js
+++ intro.js
@@ -5,11 +5,11 @@ function Translatedown()
             if (!menuexpanded) //move down if expanded
             {
               // Code for Safari
-             document.getElementById("Employers-info").style.WebkitTransform = "translateY(40px)"; 
+             document.getElementById("Employers-info").style.WebkitTransform = "translateY(80px)"; 
         // Code for IE9
-            document.getElementById("Employers-info").style.msTransform = "translateY(40px)"; 
+            document.getElementById("Employers-info").style.msTransform = "translateY(80px)"; 
         // Standard syntax
-            document.getElementById("Employers-info").style.transform = "translateY(40px)"; 
+            document.getElementById("Employers-info").style.transform = "translateY(80px)"; 
             menuexpanded=true;
 
             return;
@@ -19,11 +19,11 @@ function Translatedown()
             if(menuexpanded){
             setTimeout(function(){
             		 //move up if not expanded
-            document.getElementById("Employers-info").style.WebkitTransform = "translateY(-40px)"; 
+            document.getElementById("Employers-info").style.WebkitTransform = "translateY(-80px)"; 
         // Code for IE9
-            document.getElementById("Employers-info").style.msTransform = "translateY(-40px)"; 
+            document.getElementById("Employers-info").style.msTransform = "translateY(-80px)"; 
         // Standard syntax
-            document.getElementById("Employers-info").style.transform = "translateY(-40px)"; 
+            document.getElementById("Employers-info").style.transform = "translateY(-80px)"; 
             menuexpanded=false;
 
             return;
