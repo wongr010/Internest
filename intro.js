a58b064307e96b2d7da3f49fcd0de709568f8fab
Rosalyn <r928wong@gmail.com>
2017-06-01 11:52:58 -0400

    Content moves dynamically to make room for expanded menu button

--- intro.js
+++ intro.js
@@ -1,3 +1,38 @@
-$("li.dropdown").click(function() {
-  $("nav.navbar").toggleClass("open");
-});
+var menuexpanded=false; //intro page menu button
+
+function Translatedown()
+        {
+            if (!menuexpanded) //move down if expanded
+            {
+              // Code for Safari
+             document.getElementById("Employers-info").style.WebkitTransform = "translateY(40px)"; 
+        // Code for IE9
+            document.getElementById("Employers-info").style.msTransform = "translateY(40px)"; 
+        // Standard syntax
+            document.getElementById("Employers-info").style.transform = "translateY(40px)"; 
+            menuexpanded=true;
+
+            return;
+            
+            }
+
+            if(menuexpanded){
+            setTimeout(function(){
+            		 //move up if not expanded
+            document.getElementById("Employers-info").style.WebkitTransform = "translateY(-40px)"; 
+        // Code for IE9
+            document.getElementById("Employers-info").style.msTransform = "translateY(-40px)"; 
+        // Standard syntax
+            document.getElementById("Employers-info").style.transform = "translateY(-40px)"; 
+            menuexpanded=false;
+
+            return;
+            
+            
+
+			}, 20);
+        }
+
+            
+           
+        }
