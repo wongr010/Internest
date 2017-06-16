35e738b545f620305db45429cc54c86dc9eaa169
Rosalyn <r928wong@gmail.com>
2017-06-16 13:55:19 -0400

    Finished section 2 simulation page

--- test.js
+++ test.js
@@ -1,8 +1,30 @@
+var bubblearray=[false, false, false, false, false, false];
 
 
+function itemselected(which, index){
+	
+	bubblearray[index]=!bubblearray[index];
+	
+	if(!bubblearray[index]){
+		if(which=="like"){
+			$(event.target).removeClass("icon-plum-radio-selected");
+			$(event.target).addClass("icon-plum-radio");
+		}
+		else{
+			$(event.target).removeClass("icon-plum-radio-notlikeuslcted");
+			$(event.target).addClass("icon-plum-radio");
+		}
 
-function itemselected(){
+		return;
+	}
+	
 	$(event.target).removeClass("icon-plum-radio");
-	$(event.target).addClass("icon-plum-radio-selected");
-	console.log("added!");
+	if(which=="like"){
+		$(event.target).addClass("icon-plum-radio-selected");
+	}
+
+	if(which=="unlike"){
+		$(event.target).addClass("icon-plum-radio-notlikeuslcted");
+	}
+	
 }
